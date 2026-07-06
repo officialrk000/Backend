df28645df5e872b5ba8cc7b4eb3db3add194213e
officialrk000 <officialrk000@gmail.com>
2026-07-06 13:06:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,42 @@
-const CACHE_NAME = 'rk-verse-v1';
+const CACHE_NAME = 'rk-verse-v2';
 const urlsToCache = [
-  '.',
-  'index.html'
+  '/',
+  '/index.html'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
